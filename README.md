e5a315020ecd636889837489052ad1cc080153e9
Charlie Dedinsky <dedin005@umn.edu>
2021-12-30 23:38:50 -0600

    Push Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is a simple calculator app
+
+1. It will be able to do simple functions like multiply, subtract, add, divide
+
+2. It will be written using as few dependencies as possible to make it portable
+
+3. It will use a GUI library to do the heavy lifting, and regular calculations will be done within a class
---
+++ app.py
@@ -0,0 +1,149 @@
+import PySimpleGUI as sg
+from inspect import signature
+from collections import defaultdict
+
+class Calculator():
+    def __init__(self):
+        self.layout = [
+
+                       [sg.Text(size=(25,1),key='output')],
+                       [sg.Button('AC'),sg.Button('+/-'),sg.Button('%'),sg.Button('/')],
+                       [sg.Button('7'),sg.Button('8'),sg.Button('9'),sg.Button('X')],
+                       [sg.Button('4'),sg.Button('5'),sg.Button('6'),sg.Button('-')],
+                       [sg.Button('1'),sg.Button('2'),sg.Button('3'),sg.Button('+')],
+                       [sg.Button('0'),sg.Button('.'),sg.Button('='),sg.Button('Quit')]
+
+                      ]
+        self.window = sg.Window("Calculator", self.layout)
+        self.prev = None
+        self.d = False
+        self.later = None
+        self.save = 0
+
+        self.buttons = defaultdict(None)
+        self.buttons['AC'] = self.clear 
+        self.buttons['+/-']= self.negate 
+        self.buttons['%']  = self.percent
+        self.buttons['/']  = self.divide
+        self.buttons['X']  = self.multiply
+        self.buttons['-']  = self.subtract
+        self.buttons['+']  = self.add
+        self.buttons['=']  = self.result
+        self.buttons['.']  = self.decimal
+        self.buttons['0']  = self.update
+        self.buttons['1']  = self.update
+        self.buttons['2']  = self.update
+        self.buttons['3']  = self.update
+        self.buttons['4']  = self.update
+        self.buttons['5']  = self.update
+        self.buttons['6']  = self.update
+        self.buttons['7']  = self.update
+        self.buttons['8']  = self.update
+        self.buttons['9']  = self.update
+
+        while True:
+            event, values = self.window.read()
+
+            if event == sg.WINDOW_CLOSED or event == 'Quit':
+                break
+
+            self.func = self.buttons[event]
+
+            params = len(signature(self.func).parameters)
+            if params == 1:
+                ret = self.func(event)
+                if ret != None:
+                    self.window['output'].update('')
+                    self.update(ret)
+            elif params == 2:
+                self.run()
+                pass
+            else:
+                self.func()
+                pass
+
+            self.prev = self.func
+
+            # if event == sg.WINDOW_CLOSED or event == 'Quit':
+            #     break
+            # if event == 'AC':
+            #     print(self.window['output'])
+            #     self.clear()
+            # elif event == '':
+            #     pass
+            # elif num:
+            #     self.update(event)
+
+        self.window.close()
+
+    ####################
+    # Instance Methods #
+    ####################
+
+    # bookkeeping
+
+    # doesn't work
+    def result(self):
+        if self.later != None:
+            v = self.later(float(self.save), float(self.window['output'].get()))
+            self.clear()
+            self.update(v)
+            self.later = None
+
+    # 
+    def clear(self):
+        self.d = False
+        self.window['output'].update("0")
+
+    # 
+    def update(self, a):
+        s = self.window['output'].get()
+        if s == '0':
+            s = ''
+        self.window['output'].update(s + str(a))
+        return None
+
+    # numerical
+
+    def decimal(self):
+        if self.d:
+            return
+        self.d = True
+        self.update('.')
+
+    def percent(self):
+        a = self.window['output'].get()
+        self.window['output'].update('')
+        self.update(float(a)/100)
+
+    def negate(self):
+        a = self.window['output'].get()
+        self.window['output'].update('')
+        self.update(-float(a))
+
+    # calculations
+
+    def run(self):
+        if self.later != None:
+            self.func(float(self.save), float(self.window['output'].get()))
+        self.later = self.func
+        self.save = float(self.window['output'].get())
+        self.clear()
+
+    def multiply(self, a, b):
+        return a*b
+
+    def subtract(self, a, b):
+        return self.add(a, -b)
+
+    def add(self, a, b):
+        return a+b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError
+        return self.multiply(a, 1/b)
+
+c = Calculator()
+
+
